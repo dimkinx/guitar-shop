bc7bfb21437c1642074158f093041dba79259a9f
Dmitry Ostroukhov <ha3i6opipgmy2j8pcscvidcyq8pvca@protonmail.com>
2022-01-26 22:26:58 +0300

    Add loading screen

---
+++ src/components/loading-screen/loadingScreen.tsx
@@ -0,0 +1,23 @@
+import {Watch} from 'react-loader-spinner';
+import {LoaderParam} from '../../constants';
+import './style.css';
+
+const {Color, Size: {Width, Height}} = LoaderParam;
+
+function LoadingScreen(): JSX.Element {
+  return (
+    <>
+      <h1 className="visually-hidden">Loading screen</h1>
+      <div className="wrapper-loading">
+        <Watch
+          color={Color}
+          width={Width}
+          height={Height}
+          ariaLabel="loading"
+        />
+      </div>
+    </>
+  );
+}
+
+export default LoadingScreen;
