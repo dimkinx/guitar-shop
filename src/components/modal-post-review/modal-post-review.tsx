2b481c1cd5a8ccab7e356458f6bc741b137a4c3b
Dmitry Ostroukhov <ha3i6opipgmy2j8pcscvidcyq8pvca@protonmail.com>
2022-01-31 23:58:37 +0300

    Add review modals and auto-download reviews

---
+++ src/components/modal-post-review/modal-post-review.tsx
@@ -0,0 +1,204 @@
+import {FormEvent, useEffect, useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {postReview} from '../../store/reviews/reviews-api-actions';
+import Modal from '../modal/modal';
+import {ReviewPost} from '../../types/review';
+import {isReviewLoading, isReviewSuccess} from '../../store/reviews/reviews-selectors';
+import {setReviewStatus} from '../../store/reviews/reviews-actions';
+import {StatusType} from '../../enums';
+
+const getReviewInitialState = (productId: number): ReviewPost => ({
+  userName: '',
+  advantage: '',
+  disadvantage: '',
+  comment: '',
+  rating: 0,
+  guitarId: productId,
+});
+
+type ModalPostReviewProps = {
+  isModalOpen: boolean;
+  onModalOpenSelect: (isOpen: boolean) => void;
+  productId: number;
+  productName: string;
+}
+
+function ModalPostReview({isModalOpen, onModalOpenSelect, productId, productName}: ModalPostReviewProps): JSX.Element {
+  const isLoadingStatus = useSelector(isReviewLoading);
+  const isSuccessStatus = useSelector(isReviewSuccess);
+
+  const [review, setReview] = useState(getReviewInitialState(productId));
+  const [isChanged, setIsChanged] = useState(false);
+
+  const dispatch = useDispatch();
+
+  const handleFieldChange = (evt: {target: HTMLInputElement | HTMLTextAreaElement}) => {
+    const {name, value} = evt.target;
+    setReview({...review, [name]: name === 'rating' ? Number(value) : value});
+    setIsChanged(true);
+  };
+
+  const handleFormSubmit = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+    dispatch(postReview(review));
+  };
+
+  useEffect(() => {
+    if (isSuccessStatus) {
+      onModalOpenSelect(false);
+      setIsChanged(false);
+      setReview(getReviewInitialState(productId));
+    }
+
+    return () => {
+      dispatch(setReviewStatus(StatusType.Idle));
+    };
+  }, [isSuccessStatus]);
+
+  return (
+    <Modal
+      isModalOpen={isModalOpen}
+      onModalOpenSelect={onModalOpenSelect}
+      className="modal--review"
+    >
+      <h2 className="modal__header modal__header--review title title--medium">Оставить отзыв</h2>
+      <h3 className="modal__product-name title title--medium-20 title--uppercase">{productName}</h3>
+      <form
+        onSubmit={handleFormSubmit}
+        className="form-review"
+      >
+        <div className="form-review__wrapper">
+          <div className="form-review__name-wrapper">
+            <label className="form-review__label form-review__label--required" htmlFor="user-name">Ваше Имя</label>
+            <input
+              onChange={handleFieldChange}
+              className="form-review__input"
+              id="user-name"
+              type="text"
+              name="userName"
+              value={review.userName}
+              autoComplete="off"
+              disabled={isLoadingStatus}
+              required
+            />
+            {isChanged && !review.userName && <span className="form-review__warning">Заполните поле</span>}
+          </div>
+          <div>
+            <span className="form-review__label form-review__label--required">Ваша Оценка</span>
+            <div className="rate rate--reverse">
+              <input
+                onChange={handleFieldChange}
+                className="visually-hidden"
+                type="radio"
+                id="star-5"
+                name="rating"
+                value="5"
+                checked={review.rating === 5}
+                disabled={isLoadingStatus}
+                required
+              />
+              <label className="rate__label" htmlFor="star-5" title="Отлично" />
+              <input
+                onChange={handleFieldChange}
+                className="visually-hidden"
+                type="radio"
+                id="star-4"
+                name="rating"
+                value="4"
+                checked={review.rating === 4}
+                disabled={isLoadingStatus}
+                required
+              />
+              <label className="rate__label" htmlFor="star-4" title="Хорошо" />
+              <input
+                onChange={handleFieldChange}
+                className="visually-hidden"
+                type="radio"
+                id="star-3"
+                name="rating"
+                value="3"
+                checked={review.rating === 3}
+                disabled={isLoadingStatus}
+                required
+              />
+              <label className="rate__label" htmlFor="star-3" title="Нормально" />
+              <input
+                onChange={handleFieldChange}
+                className="visually-hidden"
+                type="radio"
+                id="star-2"
+                name="rating"
+                value="2"
+                checked={review.rating === 2}
+                disabled={isLoadingStatus}
+                required
+              />
+              <label className="rate__label" htmlFor="star-2" title="Плохо" />
+              <input
+                onChange={handleFieldChange}
+                className="visually-hidden"
+                type="radio"
+                id="star-1"
+                name="rating"
+                value="1"
+                checked={review.rating === 1}
+                disabled={isLoadingStatus}
+                required
+              />
+              <label className="rate__label" htmlFor="star-1" title="Ужасно" />
+              <span className="rate__count" />
+              {isChanged && review.rating < 1 && <span className="rate__message">Поставьте оценку</span>}
+            </div>
+          </div>
+        </div>
+        <label className="form-review__label form-review__label--required" htmlFor="advantage">Достоинства</label>
+        <input
+          onChange={handleFieldChange}
+          className="form-review__input"
+          id="advantage"
+          type="text"
+          name="advantage"
+          value={review.advantage}
+          autoComplete="off"
+          disabled={isLoadingStatus}
+          required
+        />
+        {isChanged && !review.advantage && <span className="form-review__warning">Заполните поле</span>}
+        <label className="form-review__label form-review__label--required" htmlFor="disadvantage">Недостатки</label>
+        <input
+          onChange={handleFieldChange}
+          className="form-review__input"
+          id="disadvantage"
+          type="text"
+          name="disadvantage"
+          value={review.disadvantage}
+          autoComplete="off"
+          disabled={isLoadingStatus}
+          required
+        />
+        {isChanged && !review.disadvantage && <span className="form-review__warning">Заполните поле</span>}
+        <label className="form-review__label form-review__label--required" htmlFor="comment">Комментарий</label>
+        <textarea
+          onChange={handleFieldChange}
+          className="form-review__input form-review__input--textarea"
+          id="comment"
+          rows={10}
+          name="comment"
+          value={review.comment}
+          autoComplete="off"
+          disabled={isLoadingStatus}
+          required
+        />
+        {isChanged && !review.comment && <span className="form-review__warning">Заполните поле</span>}
+        <button
+          className="button button--medium-20 form-review__button"
+          type="submit"
+          disabled={isLoadingStatus}
+        >Отправить отзыв
+        </button>
+      </form>
+    </Modal>
+  );
+}
+
+export default ModalPostReview;
---
+++ src/components/review-list/review-list.tsx
@@ -0,0 +1,136 @@
+import {useEffect, useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {fetchReviews} from '../../store/reviews/reviews-api-actions';
+import {getReviews, getReviewsTotalCount, isReviewsFailure, isReviewsIdle, isReviewsLoading, isReviewsSuccess} from '../../store/reviews/reviews-selectors';
+import Rate from '../rate/rate';
+import {addClassModifier} from '../../utils';
+import {APP_LOCALE, REVIEWS_COUNT_PER_STEP} from '../../constants';
+
+type ReviewsProps = {
+  productId: number;
+}
+
+function ReviewList({productId}: ReviewsProps): JSX.Element {
+  const reviews = useSelector(getReviews);
+  const reviewsTotalCount = useSelector(getReviewsTotalCount);
+  const isIdleStatus = useSelector(isReviewsIdle);
+  const isSuccessStatus = useSelector(isReviewsSuccess);
+  const isLoadingStatus = useSelector(isReviewsLoading);
+  const isFailureStatus = useSelector(isReviewsFailure);
+
+  const dispatch = useDispatch();
+
+  const [reviewId, setReviewId] = useState(reviews.length);
+  const [moreButton, setMoreButton] = useState<HTMLButtonElement | null>(null);
+  const [isMoreButtonShown, setIsMoreButtonShown] = useState<boolean>(true);
+
+  const handleMoreButtonClick = () => {
+    setReviewId(reviews.length);
+  };
+
+  const observer = new IntersectionObserver((entries) => {
+    const button = entries[0];
+
+    if (button.isIntersecting) {
+      setReviewId(reviews.length);
+    }
+  }, {
+    rootMargin: '0px 0px -50px',
+    threshold: 1.0,
+  });
+
+  useEffect(() => {
+    if (moreButton) {
+      observer.observe(moreButton);
+    }
+
+    return () =>  {
+      if (moreButton) {
+        observer.unobserve(moreButton);
+      }
+    };
+  });
+
+  useEffect(() => {
+    if (isSuccessStatus && reviewId + REVIEWS_COUNT_PER_STEP >= reviewsTotalCount) {
+      setIsMoreButtonShown(false);
+    }
+  }, [isSuccessStatus, reviewId, reviewsTotalCount]);
+
+  useEffect(() => {
+    if (isIdleStatus || isSuccessStatus) {
+      dispatch(fetchReviews(productId, reviewId));
+    }
+  }, [reviewId]);
+
+  if (isSuccessStatus && reviews.length === 0) {
+    return (
+      <div className="review-list">
+        <h4 className="title title--lesser">Отзывы о данном товаре - отсутствуют.</h4>
+      </div>
+    );
+  }
+
+  if (isLoadingStatus && reviews.length === 0) {
+    return (
+      <div className="review-list">
+        <h4 className="title title--lesser">Загрузка...</h4>
+      </div>
+    );
+  }
+
+  if (isFailureStatus) {
+    return (
+      <div className="review-list">
+        <h4 className="title title--lesser">Что-то пошло не так.</h4>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <ul className="review-list">
+        {reviews.map((review) => {
+          const {id, userName, advantage, disadvantage, comment: commentText, rating, createAt} = review;
+
+          return(
+            <li key={id} className="review">
+              <div className="review__wrapper">
+                <h4 className="review__title review__title--author title title--lesser">{userName}</h4>
+                <span className="review__date">
+                  {new Date(createAt).toLocaleDateString(APP_LOCALE, {
+                    day: 'numeric',
+                    month: 'long',
+                  })}
+                </span>
+              </div>
+              <Rate className="review__rating-panel" width="16" height="16" rating={rating} />
+              <h5 className="review__title title title--lesser">Достоинства:</h5>
+              <p className="review__value">{advantage}</p>
+              <h5 className="review__title title title--lesser">Недостатки:</h5>
+              <p className="review__value">{disadvantage}</p>
+              <h5 className="review__title title title--lesser">Комментарий:</h5>
+              <p className="review__value">{commentText}</p>
+            </li>
+          );
+        })}
+      </ul>
+      {isMoreButtonShown && (
+        <button
+          onClick={handleMoreButtonClick}
+          ref={setMoreButton}
+          className={`button button--medium ${addClassModifier(isLoadingStatus, 'reviews__more-button', 'loading')}`}
+          disabled={isLoadingStatus}
+        >
+          {isLoadingStatus ? (
+            <svg className="reviews__icon-loader" width="30" height="30" aria-hidden="true">
+              <use xlinkHref="#icon-oval-loader" />
+            </svg>
+          ) : 'Показать еще отзывы'}
+        </button>
+      )}
+    </>
+  );
+}
+
+export default ReviewList;
